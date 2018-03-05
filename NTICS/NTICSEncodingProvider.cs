d7ab514ad6c240f2137aa4df9437b89684e045a8
Сергій Лакас <serhiy.lakas@hotmail.com>
2018-03-05 09:54:29 +0200

    Relise 1.0

---
+++ NTICS/NTICSEncodingProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NTICS
+{
+    public class NTICSEncodingProvider : EncodingProvider
+    {
+        public override Encoding GetEncoding(int codepage)
+        {
+            if (codepage == 1251)
+                return new Encoding1251();
+            throw new NotImplementedException();
+        }
+
+        public override Encoding GetEncoding(string name)
+        {
+            if (name.ToLower() == "windows-1251")
+                return new Encoding1251();
+
+            throw new NotImplementedException();
+        }
+    }
+}
